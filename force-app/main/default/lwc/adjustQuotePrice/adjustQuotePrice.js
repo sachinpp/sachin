2b11dc9481478abd51b59c3e2b27b24572f6d63a
Sachin <sppawar2014@gmail.com>
2023-04-23 01:14:33 -0400

    Sachin Pawar Coding changes

--- force-app/main/default/lwc/adjustQuotePrice/adjustQuotePrice.js
+++ force-app/main/default/lwc/adjustQuotePrice/adjustQuotePrice.js
@@ -3,9 +3,82 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement } from "lwc";
-
-export default class AdjustQuotePrice extends LightningElement {
+import { LightningElement ,api,wire} from "lwc";
+import getQuoteDetails from '@salesforce/apex/QuoteDto.getQuoteDetails';
+import { updateRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import Amount_FIELD from '@salesforce/schema/Quote__c.TotalQuotedAmount__c';
+import ID_FIELD from '@salesforce/schema/Quote__c.Id';
+import LightningModal from 'lightning/modal';
+export default class AdjustQuotePrice extends LightningModal {
   adjustedAmountLabel = "Adjusted Amount";
-  adjustedAmount = 0;
+adjustedAmount =0;
+
+  @api recordId;
+
+  quoteData = {quoteAmount:0 };
+  error;
+  isShowModal = false;
+
+  @api
+  showModalBox() {  
+      this.isShowModal = true;
+  }
+
+  hideModalBox() {  
+      this.isShowModal = false;
+  }
+
+  handleAmountChange(event){
+    this.adjustedAmount =parseFloat(event.detail.value);
+}
+
+
+  @wire(getQuoteDetails,{recordId :'$recordId'})
+  wiredQuoteData({ error, data }) {
+      if (data) {
+          this.quoteData = data;
+          this.adjustedAmount = this.quoteData.quoteAmount;
+          this.error = undefined;
+      } else if (error) {
+          this.error = error;
+          console.log(error);
+      }
+  }
+
+  closeModal() {
+    this.close('success');
+  }
+
+  handleSave() {
+
+    const fields = {};
+    fields[ID_FIELD.fieldApiName] = this.quoteData.id;
+    fields[Amount_FIELD.fieldApiName] = this.adjustedAmount;
+
+    const recordInput = { fields };
+
+    updateRecord(recordInput)
+        .then(() => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Quote amount adjusted successfully',
+                    variant: 'success'
+                })
+            );
+            this.hideModalBox();
+        })
+        .catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error updating record',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
+        });
+    }
+
+
 }
--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -3,14 +3,89 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, api,wire,track } from "lwc";
+import getQuoteDetails from '@salesforce/apex/QuoteDto.getQuoteDetails';
+import { updateRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import STARTDATE_FIELD from '@salesforce/schema/Quote__c.StartDate__c';
+import ENDDATE_FIELD from '@salesforce/schema/Quote__c.EndDate__c';
+import ID_FIELD from '@salesforce/schema/Quote__c.Id';
 
 export default class EditQuote extends LightningElement {
   @api recordId;
-  quoteData = {
+  _startDate= 1547250828000;
+  _endDate= 1547250828000;
+  @track quoteData = {
     name: "Quote Name",
+    startDate: 1547250828000,
     endDate: 1547250828000
   };
+  error;
 
-  renderedCallback() {}
+  handleStartDateChange(event){
+  this._startDate= event.detail.value;
+}
+
+handleEndDateChange(event){
+    this._endDate = event.detail.value;
+  }
+
+  @wire(getQuoteDetails,{recordId :'$recordId'})
+  wiredQuoteData({ error, data }) {
+      if (data) {
+          this.quoteData =JSON.parse(JSON.stringify( data)) ;
+          this.error = undefined;
+      } else if (error) {
+          this.error = error;
+          console.log(error);
+      }
+  }
+  handleSave() {
+
+    const allValid = [...this.template.querySelectorAll('lightning-input')]
+    .reduce((validSoFar, inputFields) => {
+        inputFields.reportValidity();
+        return validSoFar && inputFields.checkValidity();
+    }, true);
+
+if (allValid) {
+    // Create the recordInput object
+    const fields = {};
+    fields[ID_FIELD.fieldApiName] = this.quoteData.id;
+    fields[STARTDATE_FIELD.fieldApiName] = this._startDate;
+    fields[ENDDATE_FIELD.fieldApiName] = this._endDate;
+
+    const recordInput = { fields };
+
+    updateRecord(recordInput)
+        .then(() => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Quote Details Updated Successfully',
+                    variant: 'success'
+                })
+            );
+        })
+        .catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error updating record',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
+        });
+    }
+else {
+    // The form is not valid
+    this.dispatchEvent(
+        new ShowToastEvent({
+            title: 'Something is wrong',
+            message: 'Check your input and try again.',
+            variant: 'error'
+        })
+     );
+}
+}
 }
